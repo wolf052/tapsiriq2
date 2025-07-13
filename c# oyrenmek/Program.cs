40012d5ddbe11d7c167e33417d30145876f90b10
wolf052 <cmahur108@gmail.com>
2025-07-13 22:15:27 +0400

    tapsiriq2

---
+++ c# oyrenmek/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("1ci ededi yaz: ");
+int x = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("2ci edei yaz:  ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Cemi: " + (x+y));
+Console.WriteLine("Ferq: " + (x-y));
+Console.WriteLine("Hasil: " + (x*y));
+Console.WriteLine("Nisbet: " + (x/y));
